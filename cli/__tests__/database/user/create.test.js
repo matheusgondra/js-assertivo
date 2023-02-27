8352ed96a89def0a3dc17d403cc0d160dc44aea6
Matheus de Gondra <matheusgondra951@gmail.com>
2023-02-26 21:43:34 -0300

    Finalizando capitulo 6.1

--- cli/__tests__/database/user/create.test.js
+++ cli/__tests__/database/user/create.test.js
@@ -1,6 +1,6 @@
 import { createUser } from "../../../src/database/user/create";
 import * as file from "../../../src/database/file";
-import path from "../../../src/database/path";
+import ROLES from "../../../src/constants/roles";
 
 jest.mock("../../../src/database/file");
 jest.mock("../../../src/database/path");
@@ -13,6 +13,40 @@ const usuario = {
 	lastName: "Qualquer"
 };
 
+beforeEach(() => {
+	file.loadDatabase.mockResolvedValueOnce([]);
+});
+
+afterEach(() => {
+	jest.clearAllMocks();
+});
+
+afterAll(() => {
+	jest.restoreAllMocks();
+});
+
 it("Cria usuário corretamente", async () => {
 	const user = await createUser(usuario);
+
+	expect(file.loadDatabase).toHaveBeenCalledTimes(1);
+	expect(file.saveDatabase).toHaveBeenCalledTimes(1);
+	expect(file.saveDatabase).toHaveBeenCalledWith([user]);
+	expect(user).toEqual({
+		...usuario,
+		uid: expect.any(String),
+		role: ROLES.USER
+	});
+});
+
+it("Cria usuário corretamente com role ADMIN", async () => {
+	const user = await createUser({ ...usuario, role: ROLES.ADMIN });
+
+	expect(file.loadDatabase).toHaveBeenCalledTimes(1);
+	expect(file.saveDatabase).toHaveBeenCalledTimes(1);
+	expect(file.saveDatabase).toHaveBeenCalledWith([user]);
+	expect(user).toEqual({
+		...usuario,
+		uid: expect.any(String),
+		role: ROLES.ADMIN
+	});
 });
